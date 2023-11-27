2ec7e568940c8a557c9ff979c4645ba7362a4314
Assem Mohamed <129564950+Assem29@users.noreply.github.com>
2023-11-28 01:03:40 +0200

    addSommeValidation

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:bmi_project/modules/home/bmi_home.dart';
+import 'package:bmi_project/shared/components/constent.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,124 +10,180 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+var emailControl = TextEditingController();
+var passwordControl = TextEditingController();
+var formKey = GlobalKey<FormState>();
+
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: mainColor,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Welcome in BMI App 👋',
-                style:
-                    TextStyle(fontFamily: 'SairaExtraCondensed', fontSize: 22),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Welcome in BMI App 👋',
+                  style: TextStyle(
+                      fontFamily: 'SairaExtraCondensed',
+                      fontSize: 22,
+                      color: thirdColor),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 60.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: 'Enter Your Email',
-                      labelText: 'Email',
-                      prefixIcon: const Icon(IconlyBold.user_2),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        gapPadding: 5,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: 'Enter Your Password',
-                      labelText: 'Password',
-                      prefixIcon: const Icon(IconlyBold.lock),
-                      suffixIcon: const Icon(IconlyBold.password),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: MaterialButton(
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>const BMIScreen()));
-                        },
-                        color: Colors.blue,
-                        child:  const Text(
-                          'Login',
-                          style: TextStyle(
-                              fontFamily: 'SairaExtraCondensed', fontSize: 19),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 10.0, right: 10.0, top: 60.0),
+                child: Form(
+                  key: formKey,
+                  child: Column(
                     children: [
-                      Text(
-                        'Don\'t Have any Account ? ',
-                        style: TextStyle(
-                          fontFamily: 'SairaExtraCondensed',
+                      TextFormField(
+                        controller: emailControl,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'password not be empty';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          hintText: 'Enter Your Email',
+                          labelText: 'Email',
+                          prefixIcon: const Icon(IconlyBold.user_2),
                         ),
                       ),
-                      Text(
-                        'Register Now ',
-                        style: TextStyle(
-                          fontFamily: 'SairaExtraCondensed',
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: passwordControl,
+                        obscureText: true,
+                        keyboardType: TextInputType.phone,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'password not be empty';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                            gapPadding: 5,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          hintText: 'Enter Your Password',
+                          labelText: 'Password',
+                          prefixIcon: const Icon(IconlyBold.lock),
+                          suffixIcon: const Icon(IconlyBold.password),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: MaterialButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const BMIScreen()));
+                              } else {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text(
+                                      'Something went wrong. Please try again later.'),
+                                ));
+                              }
+                            },
+                            color: Colors.purple,
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                  fontFamily: 'SairaExtraCondensed',
+                                  fontSize: 22),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Don\'t Have any Account ? ',
+                            style: TextStyle(
+                              fontFamily: 'SairaExtraCondensed',
+                              color: thirdColor,
+                            ),
+                          ),
+                          Text(
+                            'Register Now ',
+                            style: TextStyle(
+                              fontFamily: 'SairaExtraCondensed',
+                              color: thirdColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                       Text('OR',style: TextStyle(
+                        color: thirdColor,
+
+                      ),),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      MaterialButton(
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                              'assets/images/icons8-google-30.png',
+                              color: thirdColor,
+                            ),
+                            Image.asset(
+                              'assets/images/icons8-twitter-50.png',
+                              width: 30,
+                              color: thirdColor,
+                            ),
+                            Image.asset(
+                              'assets/images/icons8-facebook-50.png',
+                              width: 30,
+                              color: thirdColor,
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text('OR'),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MaterialButton(
-                    onPressed: () {},
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image.asset('assets/images/icons8-google-30.png'),
-                        Image.asset(
-                          'assets/images/icons8-twitter-50.png',
-                          width: 30,
-                        ),
-                        Image.asset(
-                          'assets/images/icons8-facebook-50.png',
-                          width: 30,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
--- lib/shared/components/constent.dart
+++ lib/shared/components/constent.dart
@@ -2,3 +2,4 @@ import 'dart:ui';
 
 Color mainColor = const Color(0xff101323);
 Color secColor = const Color(0xff202020);
+Color thirdColor =const Color(0xffffffff);
